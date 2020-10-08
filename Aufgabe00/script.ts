33f43764abab2ecdebf1d108070976c7c722d350
Sara <sara.voigt@hs-furtwangen.de>
2020-10-08 13:19:30 +0200

    new

--- Aufgabe00/script.ts
+++ Aufgabe00/script.ts
@@ -1,1 +1,7 @@
-window.prompt("Wie ist dein Name?", "");
+
+"use strict";
+var person: string = prompt("Please enter your name");
+if (person != null) {
+    document.getElementById("demo").innerHTML =
+        "Hey " + person + "! How are you doing? :)";
+}
